56dbd6597e0bf3e091f4cced463b8c2964c1f9a1
Artem Dukhnitskiy <artem.dukhnitskiy@gmail.com>
2019-04-27 12:29:21 -0700

    current changes

--- src/App.js
+++ src/App.js
@@ -1,97 +1,62 @@
 import React from 'react';
-import './App.css';
-import 'react-taco-table/dist/react-taco-table.css'
-import {
-  TacoTable,
-  DataType,
-  SortDirection,
-  Formatters,
-  Summarizers,
-  TdClassNames
-} from 'react-taco-table';
-//import './ViewDoctor';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import doctors from './data/doctors.json';
 
-import data from './data/doctors.json'
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-let doctors = JSON.stringify(data);
-
-const columns = [{
-    accessor: 'doctors.name',
-    label: 'Name',
-    priorityLevel: 1,
-    position: 1,
-    minWidth: 150,
-  },
-  {
-    accessor: 'doctors.specialty',
-    label: 'Specialty',
-    priorityLevel: 2,
-    position: 2,
-    minWidth: 150,
-  },
-  {
-    accessor: 'doctors.score',
-    label: 'Score',
-    priorityLevel: 3,
-    position: 3,
-    minWidth: 150,
-  },
-  {
-    accessor: 'doctors.area',
-    label: 'Area',
-    priorityLevel: 4,
-    position: 4,
-    minWidth: 150,
-  },
-]
+// var filtered=data.filter(function(item){
+//   return item.specialty== this.props.data;         
+// });
+// console.log(filtered);
 
 
+class BSTable extends React.Component {
+  render() {
+    const filtered = doctors.filter(x => x.specialty === Object.values(doctors).specialty)
+    console.log(this.isExpanded)
+    return (
+      <BootstrapTable data={ filtered }>
+        <TableHeaderColumn row='0' colSpan='6'>Related</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='name' isKey={ true }>Name</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='area'>Area</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='specialty'>Specialty</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='score'>Score</TableHeaderColumn>
+        <TableHeaderColumn row='1' dataField='insurance'>Insurance</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
 
-// const columns = [
-//   {
-//     id: 'name',
-//     type: DataType.String,
-//     header: 'Name',
-//     renderer(cellData, { column, rowData }) {
-//       return <a href={rowData.url} target="_blank">{cellData}</a>;
-//     },
-//   },
-//   {
-//     id: 'specialty',
-//     type: DataType.String,
-//     header: 'Specialty',
-//   },
-//   {
-//     id: 'area',
-//     type: DataType.String,
-//     header: 'Area',
-//   },
-//   {
-//     id: 'score',
-//     type: DataType.NumberOrdinal,
-//     header: 'Score',
-//   },
-//   {
-//     id: 'insurance',
-//     type: DataType.String,
-//     header: 'Insurance',
-//   }
-// ];
-//
-// class DocList extends React.Component {
-//   render() {
-//     return (
-//       <TacoTable
-//         className="doc-list"
-//         columns={columns}
-//         columnHighlighting
-//         data={data}
-//         striped
-//         sortable
-//       />
-//     );
-//   }
-// }
-//
-//
-// export default DocList;
+export default class Table extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  isExpandableRow(row) {
+    return true;
+  }
+
+  expandComponent(row) {
+    
+    return (
+      <BSTable data={ row.expand }/>
+    );
+  }
+
+  render() {
+    return (
+        <BootstrapTable ref='table' data={ doctors } bordered={ false } 
+        expandableRow={ this.isExpandableRow }
+        expandComponent={ this.expandComponent }
+        >
+          <TableHeaderColumn dataField='id' isKey={ true } >Product ID</TableHeaderColumn>
+          {/* <TableHeaderColumn dataField='name' isKey={ true } dataSort={ true }>Name</TableHeaderColumn> */}
+          <TableHeaderColumn dataField='name' dataSort={ true }>Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='area' dataSort={ true }>Area</TableHeaderColumn>
+          <TableHeaderColumn dataField='specialty' dataSort={ true }>Specialty</TableHeaderColumn>
+          <TableHeaderColumn dataField='score' dataSort={ true }>Score</TableHeaderColumn>
+          <TableHeaderColumn dataField='insurance' dataSort={ true }>Insurance</TableHeaderColumn>
+        </BootstrapTable>
+    );
+  }
+}
